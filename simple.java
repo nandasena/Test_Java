dcee816a1beca61aa94994e30f9b5c69ce5222a4
Samitha <phsnandasena060@gmail.com>
2016-06-03 11:54:43 +0530

    create java file call simple
    testing static method

---
+++ simple.java
@@ -0,0 +1,21 @@
+class simple{
+
+  static int count =0;
+
+ simple(){
+
+      count++;
+      System.out.println(count);
+  }
+
+  public static void main(String [] arg){
+
+    simple s=new simple();
+      simple a=new simple();
+        simple d=new simple();
+          simple g=new simple();
+            simple h=new simple();
+System.out.println("HEllow");
+
+  }
+}
